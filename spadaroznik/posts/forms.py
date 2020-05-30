e3021f70d6acd257d7568d9dc06b62299bf117d3
Alexey_Leuhin <forface@tut.by>
2020-05-31 00:29:38 +0300

    smth

--- spadaroznik/posts/forms.py
+++ spadaroznik/posts/forms.py
@@ -6,6 +6,7 @@ class PostForm(forms.Form):
     countries = forms.CharField(max_length=120)
     description = forms.CharField(widget=forms.Textarea)
 
+
     def save(self, commit=True):
         new_post = Publications.objects.create(countries=self.cleaned_data['countries'],
                                                description=self.cleaned_data['description'])
